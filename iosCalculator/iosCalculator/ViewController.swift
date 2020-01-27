beac51ff2b0be8d5b2c747f460a69942c9e21d43
user <user@users-iMac.local>
2020-01-27 08:23:16 -0800

    adding the function

--- iosCalculator/iosCalculator/ViewController.swift
+++ iosCalculator/iosCalculator/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var oprScreen: UILabel!
+    @IBOutlet weak var screen: UILabel!
+    
+    var flagNum = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func btnClear(_ sender: UIButton) {
+        oprScreen.text = ""
+        screen.text = ""
+    }
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        if flagNum == true {
+            screen.text = screen.text! + String(sender.tag - 1)
+            flagNum = true
+        }
+        else {
+            
+        }
+    }
+    
+    @IBAction func oprators(_ sender: UIButton) {
+        
+        switch value {
+        case <#pattern#>:
+            <#code#>
+        default:
+            <#code#>
+        }
+        
+    }
+    
 }
 
